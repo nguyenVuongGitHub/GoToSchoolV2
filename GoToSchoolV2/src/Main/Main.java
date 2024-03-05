bcc0823d50b949c8e0e87e8986d0d9acb0f67193
Harry <nguyenvuong171104@gmail.com>
2024-03-05 14:53:10 +0700

    change arvc -> argc

--- GoToSchoolV2/src/Main/Main.java
+++ GoToSchoolV2/src/Main/Main.java
@@ -3,11 +3,12 @@ package Main;
 import javax.swing.JFrame;
 
 public class Main {
-	public static void main(String[] arvc) {
+	public static void main(String[] argc) {
 		GameState gs = new GameState();
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
+
 		window.setTitle("Go To School 2.0");
 		
 		window.add(gs);
